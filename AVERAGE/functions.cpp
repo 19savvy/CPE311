142b037f4dc6d255545279c909e8f57a306c4c88
kennedy nwogu <kennedynwogu027@gmail.com>
2024-02-01 10:13:14 +0100

    fixed format of reading text file

--- AVERAGE/functions.cpp
+++ AVERAGE/functions.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 void ReadStudentInfo(Student student[]){
+	char FirstName[25], LastName[25];
 	ifstream inputFile("StudentInfo.txt");
 
 	if (!inputFile){
@@ -10,8 +11,11 @@ void ReadStudentInfo(Student student[]){
 		exit(1); // i used exit(1) to show there was an error since i cant use return because my function return type is void
 	}
 	for (int i = 0; i < numOfStudents ; i++){
-		inputFile.getline(student[i].Name, 50);
-		inputFile >> student[i].Score1 >> student[i].Score2 >> student[i].Score3 >> student[i].Score4;
+		inputFile >> FirstName >> LastName >> student[i].Score1 >> student[i].Score2 >> student[i].Score3 >> student[i].Score4;
+
+		strcpy(student[i].Name, FirstName);
+		strcat(student[i].Name, " ");
+		strcat(student[i].Name, LastName);
 		inputFile.ignore();
 	}
 	inputFile.close();
